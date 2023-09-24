a34b4f20874c493a09f1cae94f375cae90b20147
Federico Sampo <fsampo86@gmail.com>
2023-09-24 09:54:19 -0300

    Primer Escritura en el Repositorio

---
+++ JS/proyectofinal.js
@@ -0,0 +1,27 @@
+
+
+document.getElementById("boton1").addEventListener("click", function () {
+    console.log("Pagina de Facebook")
+    document.getElementById("mensaje").innerHTML = "facebook/NicholsJennie"
+    document.getElementById("mensaje").style.color = "blue"
+    });
+
+document.getElementById("boton2").addEventListener("click", function () {
+        console.log("Pagina de Instagram")
+        document.getElementById("mensaje").innerHTML = "@NicholsJennie"
+        document.getElementById("mensaje").style.color = "green"
+        });
+
+document.getElementById("boton3").addEventListener("click", function () {
+            console.log("Pagina de Linkedin")
+            document.getElementById("mensaje").innerHTML = "linkedin.com/in/JeniNi/"
+            document.getElementById("mensaje").style.color = "brown"
+            });
+   
+document.getElementById("boton4").addEventListener("click", function () {
+                console.log("Pagina de Web")
+                document.getElementById("mensaje").innerHTML = "www.jennienichols.com"
+                document.getElementById("mensaje").style.color = "black"
+                });
+    
+    
